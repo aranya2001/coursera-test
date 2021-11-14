4e2fca109da27051a50181c8b3722c18f0822d91
Aranya <72189043+aranya2001@users.noreply.github.com>
2021-11-14 22:56:05 +0530

    Update script.js

--- module5_solution/js/script.js
+++ module5_solution/js/script.js
@@ -82,8 +82,8 @@ function buildAndShowHomeHTML (categories) {
     homeHtmlUrl,
     function (homeHtml) {
       var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"randomCategoryShortName","'"+chosenCategoryShortName+"'");
-      insertHtml('#main-content',homeHtmlToInsertIntoMainPage);
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,randomCategoryShortName,"'"+chosenCategoryShortName+"'");
+      insertHtml("#main-content",homeHtmlToInsertIntoMainPage);
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
